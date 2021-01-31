5406d15e285c3a478c6529ccfe1b9e9878090449
Istayfo <i.ergun@gmx.de>
2021-01-31 14:54:24 +0100

    push

---
+++ src/components/Recipe.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import dHintergrund from '../images/dHintergrund.png'
+
+
+const NewRecipe = ({ recipe }) => {
+  console.log(recipe); // whole Object ...fields:{description,...}
+
+  const {
+    name,
+    ingredients,
+    description,
+    howtoprepare,
+    images
+  } = recipe;
+
+  return (
+    <div className="mainDiv">
+      <h2>{name}</h2>
+      
+      <div className="image">
+      <img src={`http://localhost:8080/images/${images}`} style={{width: '600px'}}/>
+      </div>
+      <div className="allRecipes">
+        <div className="description">
+          <b>Description:</b> <br />
+          <br />
+          {description}
+        </div>
+        <br />
+        <div className="ingredients">
+          <b>Ingredients:</b> <br />
+          <br />
+          {ingredients}
+        </div>
+        <br />
+        <div className="howToPrepare">
+          <b>How to prepare:</b> <br />
+          <br />
+          {howtoprepare}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewRecipe;
---
+++ src/components/Recipes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import NewRecipe from "./Recipe";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Recipes = ({ recipes }) => {
+  console.log(recipes); //array with objects...[{},{},...]=>recipes[0].fields=UgandaObject
+  const { country } = useParams();
+
+  return (
+    <div>
+      {recipes &&
+        recipes
+          .filter((recipe) => {
+            console.log(recipe);
+            console.log(country);
+          return country ? country === recipe.country : recipe;
+          })
+          .map((recipe) => {
+            console.log(recipe); //only content of Object..description, ingredients,....inside fields
+            return (
+              <div>
+                <NewRecipe recipe={recipe} />
+              </div>
+            );
+          })}
+    </div>
+  );
+};
+
+export default Recipes;
